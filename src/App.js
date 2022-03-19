dfb5589342137466b00b4aee084e46771febd019
Andres Munoz <andremude@hotmail.com>
2022-03-18 21:36:24 -0500

    compound interest data input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center">Compound Interest Calculator</h1>
+      <hr />
+      <h2 className="text-center">Data Input</h2>
+      <Form />
+      <hr />
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { useState }  from "react"
+
+
+
+const Form = () => {
+  const [state, setState] = useState();
+  const [ initialInvestment, setInitialInvestment ] = useState();
+  const [ period, setPeriod ] = useState();
+  const [ interestRate, setInterestRate ] = useState();
+  const [ monthlyContribution, setMonthlyContribution ] = useState();
+
+
+  const calculate = () => {
+    const result = initialInvestment * Math.pow(1 + interestRate / 100, period);
+    setState(result.toFixed(2));
+  };
+
+  return (
+    <section>
+      <div className="form-container">
+
+      <div className="form">
+        <div className="form-left">
+          <label htmlFor="initialInvestment">
+            Initial Investment ($) <strong>*</strong>
+            <input
+              type="number"
+              id="initialInvestment"
+              name="initialInvestment"
+              value={initialInvestment}
+              onChange={ (e) => setInitialInvestment(e.target.value) }
+            />
+          </label>
+          <label htmlFor="period">
+            Investment Period (Years) <strong>*</strong>
+            <input
+              type="number"
+              id="period"
+              name="period"
+              value={period}
+              onChange={(e) => setPeriod(e.target.value) }
+            />
+          </label>
+        </div>
+
+        <div className="form-right">
+          <label htmlFor="interestRate">
+            Annual Interest Rate (%) <strong>*</strong>
+            <input
+              type="number"
+              id="interestRate"
+              name="interestRate"
+              value={interestRate}
+              onChange={ (e) => setInterestRate(e.target.value) }
+            />
+          </label>
+          <label htmlFor="monthlyContribution">
+            Monthly Contribution ($)
+            <input
+              type="number"
+              id="monthlyContribution"
+              name="monthlyContribution"
+              value={monthlyContribution}
+              onChange={ (e) => setMonthlyContribution(e.target.value) }
+            />
+          </label>
+        </div>
+      </div>
+      <div className="bottom-container">
+        <div className="bottom-left">
+          <button type="button" className="calculate-btn" onClick={() => calculate()}>
+            Calculate
+          </button>
+        </div>
+        <div className="bottom-right">
+          <div className='result-container'>
+            $ {state}
+          </div>
+        </div>
+      </div>
+    </div>
+    </section>
+  );
+}
+
+export default Form;
